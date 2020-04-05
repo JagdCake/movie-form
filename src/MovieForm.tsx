373411e3d47c58932dcaf0e35882bdadf9343926
Ivo Angelov <jc@jagdcake.com>
2020-04-05 16:23:23 +0300

    Create movie form component

---
+++ src/MovieForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FC, ReactElement } from 'react';
+
+const MovieForm: FC = (): ReactElement => {
+    return (
+        <form></form>
+    )
+};
+
+export default MovieForm;

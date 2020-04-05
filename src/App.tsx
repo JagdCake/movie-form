5035e4aece4d55a63b3bd27e5de60ebd61211e13
Ivo Angelov <jc@jagdcake.com>
2020-04-05 16:48:10 +0300

    Display movie form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { FC, ReactElement } from 'react';
+import MovieForm from './MovieForm';
 
 const App: FC = (): ReactElement => {
     return (
         <main>
+            <MovieForm />
         </main>
     );
 }

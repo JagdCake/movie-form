136fbc9857c388dc1dfd962a3d41cc53c0e6b851
Ivo Angelov <jc@jagdcake.com>
2020-04-05 16:37:42 +0300

    Use Formik hook to set up initial form value and a basic debug function

--- src/MovieForm.tsx
+++ src/MovieForm.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { FC, ReactElement } from 'react';
+import { useFormik } from 'formik';
 
 const MovieForm: FC = (): ReactElement => {
+    const formik = useFormik({
+        initialValues: {
+            language: 'English',
+        },
+        onSubmit: (values) => {
+            console.log(JSON.stringify(values, null, 4));
+        },
+    });
+
     return (
         <form></form>
     )

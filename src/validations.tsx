3f466ae48e38d2db7c998643982b63ddaa8ce859
Ivo Angelov <jc@jagdcake.com>
2020-04-07 17:42:25 +0300

    Create form-level validation function

--- src/validations.tsx
+++ src/validations.tsx
@@ -1,3 +1,5 @@
+import { MovieFormProps } from './MovieForm';
+
 const imdbIdFormat = (imdbId: string): string => {
     const format = /^tt\d{7,8}$/;
     const error = 'Wrong format: ID must start with `tt` and end with between 7 and 8 numerical characters.';
@@ -8,3 +10,13 @@ const imdbIdFormat = (imdbId: string): string => {
 
     return '';
 };
+
+const validate = (values: MovieFormProps) => {
+    const errors: any = {};
+
+    errors.imdbId = imdbIdFormat(values.imdbId);
+
+    return errors;
+};
+
+export default validate;

d0c4888221c348da93b9f62f5ee8b7ce567441e1
Ivo Angelov <jc@jagdcake.com>
2020-04-09 20:10:08 +0300

    Create a header component
    Displays a link to the movie add form.

---
+++ src/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FC, ReactElement } from 'react';
+import { Link } from 'react-router-dom';
+
+const Header: FC = (): ReactElement => {
+    return (
+        <header className="m-auto py-12">
+            <nav>
+                <ul className="text-center text-4xl">
+                    <li>
+                        <Link
+                            to="/movies/add"
+                            className="p-4 rounded-lg bg-red-500 text-black text-center"
+                        >
+                            Add a movie!
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;

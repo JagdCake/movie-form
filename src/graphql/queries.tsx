dab3bdeb71cbc94b047a0954455cad15d0dd41ac
Ivo Angelov <jc@jagdcake.com>
2020-04-09 17:55:15 +0300

    Create type for a movie from the movie list query

--- src/graphql/queries.tsx
+++ src/graphql/queries.tsx
@@ -1,5 +1,14 @@
 import gql from 'graphql-tag';
 
+export interface Movie {
+    id: number;
+    title: string;
+    yearOfRelease: number;
+    imdbId: string;
+    myRating: string;
+    watchedOn: string;
+};
+
 export const MOVIE_LIST = gql`
     query MovieList {
         allMovies(orderBy: ID_DESC) {

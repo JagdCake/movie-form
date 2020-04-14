9142ff8bc6084a0a913a81a56e8eee9c30e6d8fc
Ivo Angelov <jc@jagdcake.com>
2020-04-14 16:06:42 +0300

    Display the movie data of the movie to update

--- src/UpdateForm.tsx
+++ src/UpdateForm.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FC, ReactElement } from 'react';
 import { useQuery } from '@apollo/react-hooks';
+import { Formik } from 'formik';
+import MovieForm from './MovieForm';
 import { MOVIE_BY_ID } from './graphql/queries';
 import QueryState from './QueryState';
 
@@ -24,7 +26,32 @@ const UpdateForm: FC<UpdateFormProp> = ({
     const movieToUpdate = data.movieById;
 
     return (
-        <>I'm an update form.</>
+        <Formik
+            initialValues={
+                {
+                    imdbId: movieToUpdate.imdbId,
+                    language: movieToUpdate.language,
+                    title: movieToUpdate.title,
+                    yearOfRelease: movieToUpdate.yearOfRelease,
+                    runtime: movieToUpdate.runtime,
+                    genre: movieToUpdate.genre,
+                    imdbRating: movieToUpdate.imdbRating,
+                    directors: movieToUpdate.directors,
+                    topActors: movieToUpdate.topActors,
+                    myRating: movieToUpdate.myRating,
+                    watchedOn: movieToUpdate.watchedOn,
+                    discussion: movieToUpdate.discussion,
+                }
+            }
+            onSubmit={(values, actions) => {
+                console.table(values);
+                actions.setSubmitting(false);
+            }}
+        >
+                {(props) => (
+                    <MovieForm {...props} />
+            )}
+        </Formik>
     );
 };
 

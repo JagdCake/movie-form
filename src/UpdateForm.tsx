158fd1922ca89cbc9511e697db181c070db9c348
Ivo Angelov <jc@jagdcake.com>
2020-04-14 10:26:35 +0300

    Create movie update form component

---
+++ src/UpdateForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FC, ReactElement } from 'react';
+
+const UpdateForm: FC = (): ReactElement => {
+    return (
+        <>I'm an update form.</>
+    );
+};
+
+export default UpdateForm;

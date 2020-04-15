330f1e2d32d6033873dfd3f49723c3c1b25440d0
Ivo Angelov <jc@jagdcake.com>
2020-04-15 15:32:33 +0300

    Conditionally render a home page link under a query state message

--- src/QueryState.tsx
+++ src/QueryState.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FC, ReactElement } from 'react';
+import { Link } from 'react-router-dom';
 
 interface QueryStateProps {
     stateMessage: string;
@@ -11,7 +12,10 @@ const QueryState: FC<QueryStateProps> = ({
     displayLinkToGoHome = false
 }: QueryStateProps): ReactElement => (
     <article className="h-screen text-white text-2xl py-64 text-center">
-        {stateMessage}
+        <h1>{stateMessage}</h1>
+        {displayLinkToGoHome === true && (
+            <Link to="/movies">&larr; Go to movie list.</Link>
+        )}
     </article>
 );
 

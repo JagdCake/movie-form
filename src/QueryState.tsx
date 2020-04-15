719f0523ed450121b196b735f7e9d81c6cc2a4cf
Ivo Angelov <jc@jagdcake.com>
2020-04-15 15:31:46 +0300

    Add prop to allow for conditional rendering of a home page link

--- src/QueryState.tsx
+++ src/QueryState.tsx
@@ -3,10 +3,12 @@ import { FC, ReactElement } from 'react';
 
 interface QueryStateProps {
     stateMessage: string;
+    displayLinkToGoHome?: boolean;
 };
 
 const QueryState: FC<QueryStateProps> = ({
     stateMessage,
+    displayLinkToGoHome = false
 }: QueryStateProps): ReactElement => (
     <article className="h-screen text-white text-2xl py-64 text-center">
         {stateMessage}

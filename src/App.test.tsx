bbfa0c91b4606dcca948a4f2d477c9c4eeac603d
Ivo Angelov <jc@jagdcake.com>
2020-04-22 16:30:28 +0300

    Create mock movie list query
    Required to be able to test the app component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { MOVIE_LIST } from './graphql/queries';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const mocks = [
+    {
+        request: {
+            query: MOVIE_LIST,
+        },
+        result: {
+            data: {
+                allMovies: {
+                    nodes: [
+                        {
+                            id: 1,
+                            title: 'Test Movie',
+                            yearOfRelease: 2020,
+                            imdbId: 'tt0000000',
+                            myRating: 'Great Test',
+                            watchedOn: '22 Apr 2020',
+                            discussion: 'link',
+                            imdbRating: '10.0',
+                            directors: ['Test Director'],
+                            topActors: ['Test Actor 1', 'Test Actor 2'],
+                            language: 'English',
+                            genre: 'Test',
+                        },
+                    ],
+                }
+            }
+        }
+    }
+];
